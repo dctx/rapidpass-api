69b79c2f88d2eba836fe9ebd08b566d245d1d464
Roy Donasco <roy.donasco@icloud.com>
2020-03-20 20:43:41 +0800

    Initial commit

---
+++ src/main/java/com/devcon/rapidpass/RapidpassApplication.java
@@ -0,0 +1,15 @@
+package com.devcon.rapidpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidpassApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(RapidpassApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/devcon/rapidpass/RapidpassApplicationTests.java
@@ -0,0 +1,15 @@
+package com.devcon.rapidpass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidpassApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
