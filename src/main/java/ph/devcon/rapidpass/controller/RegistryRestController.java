01d9574421b9a663333f7ae8b5260774bfe8dbf2
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-03-27 21:57:54 +0800

    Related to #37 - Revoke access passes. (Sets access passes status to DENIED).

---
+++ src/main/java/ph/devcon/rapidpass/controller/RegistryRestController.java
@@ -0,0 +1,62 @@
+package ph.devcon.rapidpass.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import ph.devcon.rapidpass.model.RapidPass;
+import ph.devcon.rapidpass.model.RapidPassRequest;
+import ph.devcon.rapidpass.service.RegistryService;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Registry API Rest Controller
+ */
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/registry")
+@Slf4j
+public class RegistryRestController {
+
+    private RegistryService registryService;
+
+    @Autowired
+    public RegistryRestController(RegistryService registryService) {
+        this.registryService = registryService;
+    }
+
+    @GetMapping("/accessPasses")
+    public List<RapidPass> getAccessPasses() {
+        return registryService.findAll();
+    }
+
+    @GetMapping("/accessPasses/{referenceId}")
+    RapidPass getAccessPassDetails(@PathVariable String referenceId) {
+        return registryService.find(referenceId);
+    }
+
+    @PostMapping("/accessPasses")
+    RapidPass newRequestPass(@RequestBody RapidPassRequest rapidPassRequest) {
+        return registryService.newRequestPass(rapidPassRequest);
+    }
+
+    @GetMapping("/controlCodes")
+    public Iterable<RapidPass> getControlCodes() {
+        return registryService.findAll();
+    }
+
+    @PutMapping("/accessPasses/{referenceId}")
+    RapidPass updateAccessPass(@PathVariable String referenceId, @RequestBody RapidPassRequest rapidPassRequest) {
+        return registryService.update(referenceId, rapidPassRequest);
+    }
+
+    @DeleteMapping("/accessPasses/{referenceId}")
+    HttpEntity<RapidPass> revokeAccessPass(@PathVariable String referenceId) {
+        RapidPass rapidPass = registryService.revoke(referenceId);
+        return (rapidPass == null) ? ResponseEntity.notFound().build() : ResponseEntity.ok(rapidPass);
+    }
+}
