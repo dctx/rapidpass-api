2a8bd20b5464884a4a25e4a4edc53afcf2c70440
Eric Tiongson <eric@tiongson.ph>
2020-03-27 14:53:27 +0800

    #39 WIP added registry prefix

--- src/main/java/ph/devcon/rapidpass/controller/RegistryRestController.java
+++ src/main/java/ph/devcon/rapidpass/controller/RegistryRestController.java
@@ -2,17 +2,19 @@ package ph.devcon.rapidpass.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ph.devcon.rapidpass.model.RapidPass;
 import ph.devcon.rapidpass.model.RapidPassRequest;
 import ph.devcon.rapidpass.service.RegistryService;
 
 import java.util.List;
 
+/**
+ * Registry API Rest Controller
+ */
+
 @RestController
+@RequestMapping("/registry")
 @Slf4j
 public class RegistryRestController {
 
