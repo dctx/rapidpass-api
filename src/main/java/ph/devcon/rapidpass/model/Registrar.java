4f6d9476db4b92b33083d2b3aec2405d18590f88
Eric Tiongson <eric@tiongson.ph>
2020-03-27 17:54:26 +0800

    #38 added seed data and changed id sequence generator strategy for registrar.  modified also all text columns into varchar

--- src/main/java/ph/devcon/rapidpass/model/Registrar.java
+++ src/main/java/ph/devcon/rapidpass/model/Registrar.java
@@ -8,20 +8,7 @@ package ph.devcon.rapidpass.model;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
-import javax.persistence.Basic;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 
 /**
@@ -64,9 +51,10 @@ public class Registrar implements Serializable {
 
     private static final long serialVersionUID = 1L;
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "registrar_id_generator")
+    @SequenceGenerator(name="registrar_id_generator", sequenceName = "registrar_id_seq", allocationSize=50)
     @Basic(optional = false)
-    @Column(name = "id")
+    @Column(name = "id", updatable = false, nullable = false)
     private Integer id;
     @Size(max = 150)
     @Column(name = "name")
