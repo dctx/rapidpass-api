3f34a6d5fdaa19e1f5fdbd98faf09bb9cad6c6db
Darren Karl A. Sapalo <darren.sapalo@gmail.com>
2020-03-28 19:19:17 +0800

    Fixed definitions, and fixed tests.

--- src/main/java/ph/devcon/rapidpass/entities/ControlCode.java
+++ src/main/java/ph/devcon/rapidpass/entities/ControlCode.java
@@ -11,32 +11,11 @@ import lombok.Data;
  * @see {https://gitlab.com/dctx/rapidpass/rapidpass-api/-/blob/develop/src/main/resources/rapidpass-openapi.yaml}
  */
 public class ControlCode {
-    private String passType;
-    /**
-     * Directly maps to {@link ph.devcon.rapidpass.enums.APORType} of an {@link AccessPass}, such as MED, GOV, etc.
-     */
-    private String accessType;
-    private String name;
-    private String company;
-    private String idType;
-    /**
-     * TODO: This needs to be replaced into two different fields later on, as recommended by Roy.
-     */
-    private String plateOrId;
-    private String mobileNumber;
-    private String email;
-    private String originAddress;
-    private String destAddress;
-    private String remarks;
-    private String controlCode;
-    private String status;
+    private String referenceId;
 
-    /**
-     * Assuming that this is ISO8601.
-     *
-     * Not sure how to use the date utilities class that we have.
-     */
-    private String validUntil;
+    private String controlCode;
+
+    private String passType;
 
     public static ControlCode buildFrom(AccessPass accessPass) {
 
@@ -45,17 +24,8 @@ public class ControlCode {
 
         return ControlCode.builder()
             .passType(accessPass.getPassType())
-            .accessType(accessPass.getAporType().toString())
-                .name(accessPass.getName())
-                .company(accessPass.getCompany())
-                .idType(accessPass.getIdType())
-                .plateOrId(accessPass.getPlateOrId())
-                // Cannot retrieve registrant data from access path. Or at least, it's a separate call altogether
-                .originAddress(accessPass.getOriginAddress())
-                .destAddress(accessPass.getDestinationAddress())
-                .remarks(accessPass.getRemarks())
-                .controlCode(encodedControlCode)
-                .status(accessPass.getStatus())
-                .build();
+            .referenceId(accessPass.getReferenceId())
+            .controlCode(encodedControlCode)
+            .build();
     }
 }
--- src/test/java/ph/devcon/rapidpass/controllers/RegistryRestControllerTest.java
+++ src/test/java/ph/devcon/rapidpass/controllers/RegistryRestControllerTest.java
@@ -204,7 +204,6 @@ class RegistryRestControllerTest {
                 .andDo(print());
     }
 
-
     /**
      * This tests GETting `requestPass` with either mobileNum or plateNum.
      *
@@ -214,11 +213,10 @@ class RegistryRestControllerTest {
     void getControlCodes() throws Exception {
 
         ControlCode controlCode = ControlCode.builder()
-                .accessType(APORType.MED.toString())
-                .company("DEVCON")
-                .destAddress("Somewhere in the PH")
-                .plateOrId("ABCD 1234")
-                .status(RequestStatus.APPROVED.toString()).build();
+                .controlCode("12345")
+                .passType(INDIVIDUAL.toString())
+                .referenceId("ABCDE")
+                .build();
 
         ArrayList<ControlCode> controlCodes = new ArrayList<>();
         controlCodes.add(controlCode);
@@ -236,54 +234,9 @@ class RegistryRestControllerTest {
                 // test json is expected
                 .andExpect(jsonPath("$").isArray())
                 .andExpect(jsonPath("$[0]").isMap())
-                .andExpect(jsonPath("$[0].company").value("DEVCON"))
-                .andExpect(jsonPath("$[0].destAddress").value("Somewhere in the PH"))
-                .andExpect(jsonPath("$[0].plateOrId").value("ABCD 1234"))
-                .andExpect(jsonPath("$[0].accessType").value("MED"))
-                .andExpect(jsonPath("$[0].status").value("APPROVED"))
-                .andDo(print());
-    }
-
-
-    /**
-     * This tests GETting `requestPass` with either mobileNum or plateNum.
-     *
-     * @throws Exception on failed test
-     */
-    @Test
-    void getControlCodes() throws Exception {
-
-        ControlCode controlCode = ControlCode.builder()
-                .accessType(MED.toString())
-                .company("DEVCON")
-                .destAddress("Somewhere in the PH")
-                .plateOrId("ABCD 1234")
-                .status(RequestStatus.APPROVED.toString()).build();
-
-        ArrayList<ControlCode> controlCodes = new ArrayList<>();
-        controlCodes.add(controlCode);
-
-        // mock service to return dummy INDIVIDUAL pass request when individual is request type.
-        when(mockRegistryService.getControlCodes())
-                .thenReturn(controlCodes);
-
-        final String getAccessPathUrlTemplate = "/registry/control-codes/";
-
-        JSONArray jsonArray = new JSONArray();
-        jsonArray.appendElement(controlCode);
-
-        // perform GET requestPass with mobileNum
-        mockMvc.perform(
-                get(getAccessPathUrlTemplate))
-                .andExpect(status().isOk())
-                // test json is expected
-                .andExpect(jsonPath("$").isArray())
-                .andExpect(jsonPath("$[0]").isMap())
-                .andExpect(jsonPath("$[0].company").value("DEVCON"))
-                .andExpect(jsonPath("$[0].destAddress").value("Somewhere in the PH"))
-                .andExpect(jsonPath("$[0].plateOrId").value("ABCD 1234"))
-                .andExpect(jsonPath("$[0].accessType").value("MED"))
-                .andExpect(jsonPath("$[0].status").value("APPROVED"))
+                .andExpect(jsonPath("$[0].controlCode").value("12345"))
+                .andExpect(jsonPath("$[0].passType").value("INDIVIDUAL"))
+                .andExpect(jsonPath("$[0].referenceId").value("ABCDE"))
                 .andDo(print());
     }
 
