f9cee160941b8e9e5689cd1cd1d4a5f7f9c00c25
Eric Tiongson <eric@tiongson.ph>
2020-03-28 22:19:16 +0800

    #89 added apor lookup table and seed data

---
+++ src/main/java/ph/devcon/rapidpass/entities/AporLookup.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ph.devcon.rapidpass.entities;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author eric
+ */
+@Entity
+@Table(name = "apor_lookup")
+@Data
+public class AporLookup implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 5)
+    @Column(name = "code")
+    private String code;
+    @Size(max = 50)
+    @Column(name = "description")
+    private String description;
+
+    public AporLookup() {
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (code != null ? code.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof AporLookup)) {
+            return false;
+        }
+        AporLookup other = (AporLookup) object;
+        if ((this.code == null && other.code != null) || (this.code != null && !this.code.equals(other.code))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "ph.devcon.rapidpass.entities.AporLookup[ code=" + code + " ]";
+    }
+
+}
