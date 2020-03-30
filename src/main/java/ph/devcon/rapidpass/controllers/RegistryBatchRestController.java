ab26b376fe8ce90d0a86be73adfe4deeff38dc0c
mniza <mniza@agsx.net>
2020-03-31 00:38:15 +0800

    batch approve on rapid pass request

--- src/main/java/ph/devcon/rapidpass/controllers/RegistryBatchRestController.java
+++ src/main/java/ph/devcon/rapidpass/controllers/RegistryBatchRestController.java
@@ -1,16 +1,23 @@
 package ph.devcon.rapidpass.controllers;
 
 
+import com.opencsv.bean.ColumnPositionMappingStrategy;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import ph.devcon.rapidpass.models.RapidPass;
 import ph.devcon.rapidpass.models.RapidPassBatchRequest;
+import ph.devcon.rapidpass.models.RapidPassCSVdata;
 import ph.devcon.rapidpass.services.RegistryService;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.List;
 
 
 /**
@@ -32,16 +39,60 @@ public class RegistryBatchRestController {
     /**
      * Upload CSV or excel file of approved control numbers
      *
-     * TODO: This endpoint needs to be reworked to support receiving file data.
+     * @param csvFile Receives CSV File Payload
+     * @param username Registrar User name
      *
-     * @param rapidPassBatchRequest THIS IS INCORRECT, as the batch data should not be delivered by JSON request body,
-     *                              but instead by file upload data.
      */
     @PostMapping("/access-passes")
-    Iterable<RapidPass> newRequestPass(@RequestBody RapidPassBatchRequest rapidPassBatchRequest) {
-        // todo: implement on registry service
-        return this.registryService.batchUpload(rapidPassBatchRequest);
+    Iterable<RapidPass> newRequestPass(@RequestParam("file") MultipartFile csvFile)
+            throws IOException, RegistryService.UpdateAccessPassException {
+
+        List<RapidPassCSVdata> approvedAccessPass;
+
+        if (csvFile.isEmpty()) {
+            return null;
+        } else {
+
+            try (Reader fileReader = new BufferedReader(new InputStreamReader(csvFile.getInputStream()))) {
+                ColumnPositionMappingStrategy strategy = new ColumnPositionMappingStrategy();
+                strategy.setType(RapidPassCSVdata.class);
+                String[] accessPassCSVColumnMapping = {
+                        "passType",
+                        "aporType",
+                        "firstName",
+                        "middleName",
+                        "lastName",
+                        "suffix",
+                        "company",
+                        "idType",
+                        "identifierNumber",
+                        "mobileNumber",
+                        "email",
+                        "originName",
+                        "originStreet",
+                        "originCity",
+                        "destName",
+                        "destStreet",
+                        "destCity",
+                        "remarks"
+                };
+
+                strategy.setColumnMapping(accessPassCSVColumnMapping);
+                CsvToBean<RapidPassCSVdata> csvToBean = new CsvToBeanBuilder(fileReader)
+                        .withMappingStrategy(strategy)
+                        .withType(RapidPassCSVdata.class)
+                        .withSkipLines(1)
+                        .withIgnoreLeadingWhiteSpace(true)
+                        .build();
+
+                approvedAccessPass = csvToBean.parse();
+
+                fileReader.close();
+            } catch (Exception e) {
+                throw e;
+            }
+        }
+        return this.registryService.batchUpload(approvedAccessPass);
     }
-
-
 }
+
--- src/main/java/ph/devcon/rapidpass/services/RegistryService.java
+++ src/main/java/ph/devcon/rapidpass/services/RegistryService.java
@@ -10,12 +10,14 @@ import ph.devcon.rapidpass.entities.Registrar;
 import ph.devcon.rapidpass.enums.AccessPassStatus;
 import ph.devcon.rapidpass.models.RapidPass;
 import ph.devcon.rapidpass.models.RapidPassBatchRequest;
+import ph.devcon.rapidpass.models.RapidPassCSVdata;
 import ph.devcon.rapidpass.models.RapidPassRequest;
 import ph.devcon.rapidpass.repositories.AccessPassRepository;
 import ph.devcon.rapidpass.repositories.RegistrantRepository;
 import ph.devcon.rapidpass.repositories.RegistryRepository;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -120,6 +122,7 @@ public class RegistryService {
         accessPass.setDateTimeCreated(now);
         accessPass.setDateTimeUpdated(now);
         accessPass.setStatus("PENDING");
+        accessPass.setRemarks(rapidPassRequest.getRemarks());
 
         log.debug("Persisting Registrant: {}", registrant.toString());
         accessPass = accessPassRepository.saveAndFlush(accessPass);
@@ -192,6 +195,7 @@ public class RegistryService {
         accessPass.setOriginName(rapidPassRequest.getOriginName());
         accessPass.setOriginCity(rapidPassRequest.getOriginCity());
         accessPass.setOriginStreet(rapidPassRequest.getOriginStreet());
+        accessPass.setRemarks(rapidPassRequest.getRemarks());
 
         accessPass.setIdentifierNumber(rapidPassRequest.getIdentifierNumber());
         accessPass.setIdType(rapidPassRequest.getIdType());
@@ -284,18 +288,6 @@ public class RegistryService {
         return RapidPass.buildFrom(accessPass);
     }
 
-    /**
-     * Returns a list of rapid passes that were requested for granting or approval.
-     * <p>
-     * TODO: This needs to be reworked such that the batch data is not uploaded via json request body, but by excel file or csv.
-     *
-     * @param rapidPassBatchRequest JSON object containing an array of rapid pass requests
-     * @return a list of generated rapid passes, whose status are all pending (because they have just been requested).
-     */
-    public Iterable<RapidPass> batchUpload(RapidPassBatchRequest rapidPassBatchRequest) {
-        // TODO: implement
-        return null;
-    }
 
     public RapidPass updateAccessPass(String referenceId, RapidPass rapidPass) throws UpdateAccessPassException {
         final RapidPass updatedRapidPass;
@@ -315,6 +307,30 @@ public class RegistryService {
 
         return updatedRapidPass;
     }
+
+
+    /**
+     * Returns a list of rapid passes that were requested for granting or approval.
+     *
+     * @param approvedRapidPasses  Iterable<RapidPass> of Approved passes application
+     * @return a list of generated rapid passes, whose status are all approved.
+     */
+    public List<RapidPass> batchUpload(List <RapidPassCSVdata> approvedRapidPasses) throws RegistryService.UpdateAccessPassException {
+
+        log.info("Process Batch Approving of AccessPass");
+        List <RapidPass> passes = new ArrayList<RapidPass>();
+        RapidPass pass;
+        for ( RapidPassCSVdata rapidPassRequest: approvedRapidPasses) {
+            pass = this.newRequestPass(RapidPassRequest.buildFrom(rapidPassRequest));
+
+            if ( pass != null ){
+                pass = this.grant(rapidPassRequest.getMobileNumber());
+                passes.add(pass);
+            }
+        }
+        return passes;
+    }
+
 
     /**
      * This is thrown when updates are not allowed for the AccessPass.
