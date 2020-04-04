752dc41b5427c029568e94a155b5865ff34e5487
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-04 20:55:20 +0800

    Added new fields for AgencyUser, to be used by the batch upload code.
    Data model change- instead of the lower case string "active", the new `status` values for an AgencyUser is "ACTIVE" or "INACTIVE", as defined by the enum.

--- src/main/java/ph/devcon/rapidpass/models/AgencyUser.java
+++ src/main/java/ph/devcon/rapidpass/models/AgencyUser.java
@@ -2,15 +2,58 @@ package ph.devcon.rapidpass.models;
 
 import lombok.Builder;
 import lombok.Data;
+import ph.devcon.rapidpass.entities.Registrar;
+import ph.devcon.rapidpass.entities.RegistrarUser;
+import ph.devcon.rapidpass.enums.RegistrarUserSource;
 
+/**
+ * This data model maps out to the database table {@link RegistrarUser}.
+ */
 @Data
 @Builder
 public class AgencyUser {
 
-    // just a subset for now
-
+    /**
+     * The {@link Registrar} or agency that this user is associated with.
+     */
     private String registrar;
+
+    /**
+     * The user's personal username.
+     */
     private String username;
+
+    /**
+     * The user's personal password.
+     */
     private String password;
 
+    /**
+     * The user's email.
+     */
+    private String email;
+
+    /**
+     * Can be "INDIVIDUAL" or "BATCH_UPLOAD";
+     *
+     * Please see {@link RegistrarUserSource}.
+     */
+    private String source;
+
+
+    private String firstName;
+
+    private String lastName;
+
+    /**
+     * If the source value of this AgencyUser is null, then it defaults to an individual (single) registration.
+     * @return true if the AgencyUser was generated from batch upload.
+     */
+    public boolean isBatchUpload() {
+        return RegistrarUserSource.BATCH_UPLOAD.toString().equalsIgnoreCase(this.source);
+    }
+
+    public boolean isIndividualRegistration() {
+        return !isBatchUpload();
+    }
 }
