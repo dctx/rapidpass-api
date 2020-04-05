43f7b664743970841ce9dda0c964b957c2e5dbc6
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-05 19:55:31 +0800

    Added new query filter parameters.

--- src/main/java/ph/devcon/rapidpass/models/QueryFilter.java
+++ src/main/java/ph/devcon/rapidpass/models/QueryFilter.java
@@ -3,7 +3,11 @@ package ph.devcon.rapidpass.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import ph.devcon.rapidpass.api.models.CommonRapidPassFields;
+import ph.devcon.rapidpass.enums.AccessPassStatus;
+import ph.devcon.rapidpass.enums.PassType;
 import ph.devcon.rapidpass.enums.RecordSource;
+import ph.devcon.rapidpass.utilities.StringFormatter;
 
 @Data
 @NoArgsConstructor
@@ -12,12 +16,63 @@ public class QueryFilter {
 
     public static final Integer DEFAULT_PAGE_SIZE = 25;
 
+    /**
+     * Allows a user to filter by pass type.
+     *
+     * For possible values, see {@link PassType}.
+     */
     private String passType;
+    /**
+     * Allows a user to filter by Apor type.
+     *
+     * For possible values, trust the db data primarily. Note that {@link CommonRapidPassFields.AporTypeEnum} may
+     * potentially be outdated, as it is based on the OpenAPI Spec. Again, trust the db first and foremost.
+     */
     private String aporType;
+
+    /**
+     * Allows a user to filter access passes based on reference ID.
+     */
     private String referenceId;
+
+    /**
+     * Allows a user to filter by status.
+     *
+     * For possible values, see {@link AccessPassStatus}.
+     */
     private String status;
+
+    /**
+     * Allows a user to filter by plateNumber.
+     *
+     * Note that plate numbers are normalized using {@link StringFormatter#normalizeAlphanumeric(String)}.
+     */
     private String plateNumber;
-    private RecordSource source; // ONLINE, BULK_UPLOAD
+
+    /**
+     * Allows a user to filter by company name.
+     */
+    private String company;
+
+    /**
+     * Allows a user to filter by access pass name.
+     */
+    private String name;
+
+    /**
+     * Allows a user to filter by source.
+     *
+     * For possible values, see {@link RecordSource}.
+     */
+    private RecordSource source;
+
+    /**
+     * Allows a user to paginate the results. This specifies which page will be shown.
+     */
     private Integer pageNo = 0;
+
+    /**
+     * Allows a user to paginate the results. This specifies how many results per page.
+     */
     private Integer maxPageRows = DEFAULT_PAGE_SIZE;
 }
