b4fe56bf0a1c38e236c5bc9ef050a38dc4acbb7b
Jonah Jose Espelita <a0284021@ti.com>
2020-04-10 17:49:18 +0800

    Fixed rbac auth for `GET /registry/access-passes` added rbac unit tests

--- src/test/java/ph/devcon/rapidpass/config/SimpleRbacConfigTest.java
+++ src/test/java/ph/devcon/rapidpass/config/SimpleRbacConfigTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
@@ -23,10 +25,34 @@ class SimpleRbacConfigTest {
 
     @Test
     void loadConfigProps() {
-        log.info("got {}", simpleRbacConfig);
+        log.info("loaded rbac {}", simpleRbacConfig);
         assertThat(simpleRbacConfig.getRoles(), is(not(empty())));
 
     }
 
+    @Test
+    void getRbacRoleMatch_01() {
+        final List<SimpleRbacConfig.RbacRole> test =
+                simpleRbacConfig.getRbacRoleMatch("/registry/access-passes/12345", "DELETE");
+        assertThat(test.get(0).getRole(), is("approver"));
 
+
+    }
+
+
+    @Test
+    void getRbacRoleMatch_02() {
+        final List<SimpleRbacConfig.RbacRole> test =
+                simpleRbacConfig.getRbacRoleMatch("/registry/access-passes", "GET");
+        assertThat(test, is(not(empty())));
+        assertThat(test.get(0).getRole(), is("approver"));
+    }
+
+    @Test
+    void getRbacRoleMatch_03() {
+        final List<SimpleRbacConfig.RbacRole> test =
+                simpleRbacConfig.getRbacRoleMatch("/registry/access-passes/12345", "GET");
+        assertThat("GET /registry/access-passes/12345 should be public", test, is(empty()));
+
+    }
 }
