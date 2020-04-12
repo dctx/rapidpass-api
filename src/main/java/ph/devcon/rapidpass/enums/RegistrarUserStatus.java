d66c6942e718c73e4df53ece9b234658cce47454
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-12 15:30:41 +0800

    Added inline documentation.

--- src/main/java/ph/devcon/rapidpass/enums/RegistrarUserStatus.java
+++ src/main/java/ph/devcon/rapidpass/enums/RegistrarUserStatus.java
@@ -1,10 +1,24 @@
 package ph.devcon.rapidpass.enums;
 
+import ph.devcon.rapidpass.entities.RegistrarUser;
+
 public enum RegistrarUserStatus {
-    // Refers to a RegistrarUser who is allowed to log in already. This means that their password is configured.
+    /**
+     * <p>
+     * Refers to a {@link RegistrarUser} who has its authentication details configured (password, username) and
+     * is allowed to log in already.
+     * </p>
+     */
     ACTIVE,
 
-    // Refers to a RegistrarUser who was registered using batch upload, and does not have their password configured yet.
-    // This means that an email was sent to them first, to have them activate and configure their password.
+    /**
+     * <p>
+     * Refers to a {@link RegistrarUser} who was registered using batch upload, and does not have their password
+     * configured yet.
+     * </p>
+     * <p>
+     * This means that an email was sent to them first, to have them activate and configure their password.
+     * </p>
+     */
     INACTIVE
 }
