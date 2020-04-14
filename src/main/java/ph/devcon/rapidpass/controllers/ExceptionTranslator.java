20d8a8c23f141343f3e5d66ccdd2aef149a3bcb3
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-14 20:03:37 +0800

    Closes #353 - Handle enum parsing errors elegantly.

--- src/main/java/ph/devcon/rapidpass/controllers/ExceptionTranslator.java
+++ src/main/java/ph/devcon/rapidpass/controllers/ExceptionTranslator.java
@@ -15,10 +15,12 @@
 package ph.devcon.rapidpass.controllers;
 
 import com.fasterxml.jackson.databind.exc.InvalidFormatException;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
@@ -29,10 +31,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import ph.devcon.rapidpass.exceptions.AccountLockedException;
 import ph.devcon.rapidpass.services.RegistryService;
+import ph.devcon.rapidpass.validators.InvalidEnumValueException;
 
 import javax.validation.ConstraintViolationException;
-import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.toMap;
 
@@ -95,6 +99,42 @@ public class ExceptionTranslator {
     public Map<String, String> illegalArgsError(Throwable ex) {
         log.warn("Request Error! ", ex);
         return ImmutableMap.of("message", ex.getMessage());
+    }
+
+    /**
+     * Generic error catcher so that we don't print stack traces back to client.
+     *
+     * @param ex generic error
+     * @return respoinse body with errors
+     */
+    @ExceptionHandler({HttpMessageNotReadableException.class})
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseBody
+    public Map<String, ?> handleEnumParsingFailures(HttpMessageNotReadableException ex) {
+        // Cannot deserialize value of type `ph.devcon.rapidpass.enums.PassType` from String "<string>": not one of the values accepted for Enum class: [INDIVIDUAL, VEHICLE]
+        Throwable cause = ex.getCause();
+        if (cause instanceof InvalidFormatException) {
+            InvalidFormatException invalidFormatException = (InvalidFormatException) cause;
+
+            List<String> errors = invalidFormatException.getPath()
+                    .stream()
+                    .map(reference -> {
+                        String key = reference.getFieldName();
+                        String value = (String) invalidFormatException.getValue();
+
+                        ImmutableList<Object> objects = ImmutableList.copyOf(invalidFormatException.getTargetType().getEnumConstants());
+                        List<String> values = objects.stream().map(Object::toString).collect(Collectors.toList());
+
+                        return new InvalidEnumValueException(value, key, values);
+                    })
+                    .map(Throwable::getMessage)
+                    .collect(Collectors.toList());
+
+
+            return ImmutableMap.of("message", "Unexpected values for an enum could not be parsed.", "errors", errors);
+        }
+
+        return everythingElse(ex);
     }
 
     /**
