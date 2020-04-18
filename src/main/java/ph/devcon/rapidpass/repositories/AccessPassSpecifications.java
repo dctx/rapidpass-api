3b3a5c3199888230c650b8cfb0282db1d84eb830
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-18 18:37:42 +0800

    Related to #354 - Use case insensitive search.

--- src/main/java/ph/devcon/rapidpass/repositories/AccessPassSpecifications.java
+++ src/main/java/ph/devcon/rapidpass/repositories/AccessPassSpecifications.java
@@ -21,6 +21,7 @@ import ph.devcon.rapidpass.entities.AccessPass;
 import ph.devcon.rapidpass.models.QueryFilter;
 
 import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.Expression;
 import javax.persistence.criteria.Predicate;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -86,9 +87,13 @@ public class AccessPassSpecifications {
         return (((root, criteriaQuery, criteriaBuilder) -> {
             if (StringUtils.isBlank(search))
                 return null;
+
+            Expression<String> lowerName = criteriaBuilder.function("lower", String.class, root.get("name"));
+            Expression<String> lowerCompany = criteriaBuilder.function("lower", String.class, root.get("company"));
+
             return criteriaBuilder.or(
-                    criteriaBuilder.like(root.get("company"), "%"+ search +"%"),
-                    criteriaBuilder.like(root.get("name"), "%"+ search +"%")
+                    criteriaBuilder.like(lowerCompany, "%"+StringUtils.lowerCase(search)+"%"),
+                    criteriaBuilder.like(lowerName, "%"+StringUtils.lowerCase(search)+"%")
             );
         }));
     }
