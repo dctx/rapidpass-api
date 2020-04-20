a05aa4ec2850bcbf6f8719b44be420e5e9245281
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-21 00:53:52 +0800

    Added security filters.

---
+++ src/main/java/ph/devcon/rapidpass/filters/ResponseHeaderSecurityFilter.java
@@ -0,0 +1,37 @@
+package ph.devcon.rapidpass.filters;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ResponseHeaderSecurityFilter implements Filter {
+
+    @Value("${security.csp.srcs.script:*.rapidpass.ph rapidpass.ph}")
+    private String source;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        response.addHeader("Strict-Transport-Security", "max-age=15552000; includeSubDomains; preload");
+        response.addHeader("Content-Security-Policy", "default-src 'self'; script-src 'report-sample' 'self' https://www.googletagmanager.com/gtag/js; style-src 'report-sample' 'self' https://fonts.googleapis.com; object-src 'none'; base-uri 'self'; connect-src 'self' " + source + "; font-src 'self' https://fonts.gstatic.com; frame-src 'self'; img-src 'self'; manifest-src 'self'; media-src 'self'; worker-src 'none';");
+        response.addHeader("X-Frame-Options", "SAMEORIGIN");
+        response.addHeader("X-Content-Type-Options", "nosniff");
+        response.addHeader("cache-control", "max-age=0, private, must-revalidate");
+        response.addHeader("Referrer-Policy", "no-referrer");
+        response.addHeader("Feature-Policy", "none");
+
+        filterChain.doFilter(servletRequest, response);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
