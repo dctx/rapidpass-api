4830d0d0e4a439546e519058785969d56d168698
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-22 05:09:23 +0800

    Updated validators.

--- src/main/java/ph/devcon/rapidpass/utilities/validators/entities/accesspass/rules/IsValidPassType.java
+++ src/main/java/ph/devcon/rapidpass/utilities/validators/entities/accesspass/rules/IsValidPassType.java
@@ -2,17 +2,11 @@ package ph.devcon.rapidpass.utilities.validators.entities.accesspass.rules;
 
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
-import ph.devcon.rapidpass.enums.PassType;
 import ph.devcon.rapidpass.models.RapidPassRequest;
-
-import java.util.List;
 
 public class IsValidPassType implements Validator {
 
-    final private List<PassType> passType;
-
-    public IsValidPassType(List<PassType> aporTypes) {
-        this.passType = aporTypes;
+    public IsValidPassType() {
     }
 
     @Override
@@ -21,20 +15,15 @@ public class IsValidPassType implements Validator {
     }
 
     protected boolean isValidPassType(String passType) {
-        try {
-            PassType.valueOf(passType);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
+        return passType.equals("INDIVIDUAL") || passType.equals("VEHICLE");
     }
 
     @Override
     public void validate(Object object, Errors errors) {
-        RapidPassRequest request = object instanceof RapidPassRequest ? ((RapidPassRequest) object) : null;
-        if (request == null) return;
+        RapidPassRequest rapidPassRequest = object instanceof RapidPassRequest ? ((RapidPassRequest) object) : null;
 
-        if (request.getPassType() == null || !isValidPassType(request.getPassType().name()))
+        if (rapidPassRequest != null && (rapidPassRequest.getPassType() == null || !isValidPassType(rapidPassRequest.getPassType().name())))
             errors.rejectValue("passType", "invalid.passType", "Invalid Pass Type.");
+
     }
 }
