971173384f1fd0e98a4d374464252ff06886ec41
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-04-28 23:41:15 +0800

    Add cast checking before casting.

--- src/main/java/ph/devcon/rapidpass/services/RegistryService.java
+++ src/main/java/ph/devcon/rapidpass/services/RegistryService.java
@@ -198,14 +198,14 @@ public class RegistryService {
         accessPass.setValidFrom(now);
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentLoggedInUser = null;
 
         if (authentication.getPrincipal() instanceof HashMap) {
             HashMap<String, Object> principal = (HashMap<String, Object>) authentication.getPrincipal();
-            String currentLoggedInUser = principal.get("sub").toString();
-
-            accessPass.setIssuedBy(currentLoggedInUser);
+            currentLoggedInUser = principal.get("sub").toString();
         }
 
+        accessPass.setIssuedBy(currentLoggedInUser);
         if (status == AccessPassStatus.PENDING) {
             // just set the validity period to today until the request is approved
             accessPass.setValidTo(now);
@@ -571,6 +571,13 @@ public class RegistryService {
         // Validation
         BatchAccessPassRequestValidator batchAccessPassRequestValidator = new BatchAccessPassRequestValidator(this.lookupTableService, this.accessPassRepository);
 
+        String currentLoggedInUser = null;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication instanceof HashMap) {
+            HashMap<String, Object> principal = (HashMap<String, Object>) authentication.getPrincipal();
+            currentLoggedInUser = principal.get("sub").toString();
+        }
+
         RapidPass pass;
         int counter = 1;
         Instant start = Instant.now();
@@ -645,12 +652,6 @@ public class RegistryService {
 
                             AccessPass latestPendingAccessPass = accessPasses.remove(0);
 
-                            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-                            String currentLoggedInUser = null;
-                            if (authentication.getPrincipal() instanceof HashMap) {
-                                HashMap<String, Object> principal = (HashMap<String, Object>) authentication.getPrincipal();
-                                currentLoggedInUser = principal.get("sub").toString();
-                            }
 
                             // let's approve all pending requests with the same reference id and pass type
                             for (AccessPass accessPass : accessPasses) {
@@ -730,10 +731,12 @@ public class RegistryService {
     private void updateAccessPassValidityToDefault(AccessPass accessPass) {
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        HashMap<String, Object> principal = (HashMap<String, Object>) authentication.getPrincipal();
-        String currentLoggedInUser = principal.get("sub").toString();
+        if (authentication.getPrincipal() instanceof HashMap) {
+            HashMap<String, Object> principal = (HashMap<String, Object>) authentication.getPrincipal();
+            String currentLoggedInUser = principal.get("sub").toString();
 
-        accessPass.setIssuedBy(currentLoggedInUser);
+            accessPass.setIssuedBy(currentLoggedInUser);
+        }
 
         accessPass.setValidTo(getExpirationDate());
 
