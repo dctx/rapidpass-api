9420671b666def7115e1f28c568ff77650f7d696
Darren Karl Sapalo <darren.sapalo@gmail.com>
2020-05-15 17:53:49 +0800

    Closes #454 - Updated apor_lookup table. Created new JPA entity and repository.

---
+++ src/main/java/ph/devcon/rapidpass/entities/AporLookup.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2020.  DevConnect Philippines, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package ph.devcon.rapidpass.entities;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
+/**
+ *
+ * @author darren
+ */
+@Entity
+@Table(name = "apor_lookup")
+@Data
+public class AporLookup implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @Size(max = 10)
+    @Column(name = "code")
+    private String aporCode;
+
+    @Size(max = 100)
+    @Column(name = "description")
+    private String description;
+
+    @Size(max = 100)
+    @Column(name = "approving_agency")
+    private String approvingAgency;
+
+    public AporLookup() {
+
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (object instanceof AporLookup) {
+            AporLookup aporLookup = (AporLookup) object;
+            if (aporLookup.aporCode == null && this.aporCode == null) return true;
+            if (aporLookup.aporCode != null && this.aporCode != null) return aporLookup.aporCode.equals(this.aporCode);
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "ph.devcon.rapidpass.entities.AporLookUp[ aporCode=" + this.aporCode + " ]";
+    }
+    
+}
