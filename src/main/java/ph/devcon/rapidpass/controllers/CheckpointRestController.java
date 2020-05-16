a0da848ac5b6112eb50d02e5dd09483f7c68252a
Eric Tiongson <eric@tiongson.ph>
2020-05-16 23:34:37 +0800

    updated note

--- src/main/java/ph/devcon/rapidpass/controllers/CheckpointRestController.java
+++ src/main/java/ph/devcon/rapidpass/controllers/CheckpointRestController.java
@@ -209,7 +209,7 @@ public class CheckpointRestController {
         claims.put("xsrfToken", UUID.randomUUID().toString());
         claims.put("exp", expiry.toEpochSecond());
 
-        // FIXME this won't work with new keycloak implementation!
+        // JWT is currently not used by the checkpoint app
 //        String jwt = JwtGenerator.generateToken(claims, this.jwtSecretsConfig.findGroupSecret(JWT_GROUP));
 
         CheckpointAuthResponse authResponse = CheckpointAuthResponse.builder()
